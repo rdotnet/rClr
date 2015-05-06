e5f53b7429cc26d3e8f381e618d7c4cc3ebb8997
J-M <per202@csiro.au>
2015-05-06 18:59:23 +1000

    Fix vignettes to work with knitr 1.10. Candidate 0.7-6

--- R/rClr-package.r
+++ R/rClr-package.r
@@ -6,20 +6,21 @@
 #' \tabular{ll}{
 #' Package: \tab rClr\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.7-5\cr
-#' Purpose: \tab 0.7-5 is not yet released. Compile against mono sgen rather than boehm.. \cr
+#' Version: \tab 0.7-6\cr
+#' Notes: \tab update the R Markdown vignettes to work with the latest version of knitr \cr
 #' Date: \tab 2015-04-06\cr
 #' License: \tab LGPL 3\cr
 #' }
 #'
-#' 
-#' rClr is a package for low-level access from R to a Common Language
-#' Runtime (CLR). The supported CLR implementations are Microsoft '.NET'
-#' framework on Windows and Mono on several platforms, currently Windows and at least 
-#' Debian Linux. rClr has been used at least in scientific modelling work since
-#' 2013-06. Running it on Mono has been relatively experimental, but as of November 2014 
-#' several factors have enabled very near functional parity with the Windows/.NET.
-#' 
+#' rClr is a package for low-level access from R to a Common Language Runtime (CLR). The supported CLR implementations are Microsoft '.NET'
+#' framework on Windows and Mono on several platforms, currently Windows and at least Debian Linux. rClr has been used in scientific modelling work since
+#' 2013-06. Running it on Mono has been relatively experimental, but as of November 2014 several factors have enabled very near functional parity with the Windows/.NET.
+#'
+#' \tabular{lll}{
+#' Version \tab Date \tab Notes \cr
+#' 0.7-5: \tab N/A \tab Internal release for work project. Compile against mono sgen rather than boehm. \cr
+#' }
+#'
 #' I gratefully acknowledge Kosei Abe and others for their work on the dependency R.NET (see https://github.com/jmp75/rdotnet/graphs/contributors)
 #'
 #' @name rClr-package
