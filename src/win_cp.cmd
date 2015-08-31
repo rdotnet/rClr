d02f7b0a7678af1bb7b844dce866471dfba3270d
J-M <jean-michel.perraud@csiro.au>
2015-08-31 11:37:42 +1000

    Progress new release on Windows

--- src/win_cp.cmd
+++ src/win_cp.cmd
@@ -2,5 +2,20 @@
 REM This utility uses the native windows xcopy to overcome an issue that can arise with 'cp' coming with MinGW.
 set unix_args=%*
 set dos_paths=%unix_args:/=\%
-@echo robocopy %dos_paths%
-robocopy %dos_paths%
+robocopy /MIR /MT:1 /R:2 /NJS /NJH /NFL /NDL /XX %dos_paths%
+@set exit_code=0
+
+REM http://ss64.com/nt/robocopy-exit.html
+@if errorlevel 0 goto exit
+@if errorlevel 1 goto exit
+
+goto robocopy_fail
+
+:robocopy_fail
+@echo ERROR: robocopy exit code %errorlevel%. See http://ss64.com/nt/robocopy-exit.html for details.
+@set exit_code=1
+@goto exit
+
+:exit
+exit /b %exit_code%
+
